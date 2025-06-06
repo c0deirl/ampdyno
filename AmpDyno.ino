8c02762adac337b2d546f1c86cf8dc82db2f73f9
Matthew Greathouse <132587783+c0deirl@users.noreply.github.com>
2025-06-06 11:32:21 -0400

    General cleanup

--- AmpDyno.ino
+++ AmpDyno.ino
@@ -5,77 +5,67 @@
  * Designed by M Greathouse
  * Current Sensor Input -> A2
  * Temperature Sensor Input -> D7
- * Reset button input - A1
- * 4-8ohm Switch Input - D12
+ * Reset button input - D8
+ * 4-8ohm Load Bank Switch Input - D12
  */
 
-#include <LiquidCrystal.h>  //Default Arduino LCD Library is included 
-#include <LiquidCrystal_I2C.h> // Library to use I2C displays
-#include <DHT.h> // Define library for temp sensor
+#include <LiquidCrystal.h>      //Default Arduino LCD Library is included 
+#include <LiquidCrystal_I2C.h>  // Library to use I2C displays
+#include <DHT.h>                // Define library for temp sensor
 
-#define DHT11_PIN 7 // USE PIN 7 Digital for the DHT11 temp sensor
-#define DHTTYPE DHT11 // DHT 11 for actual use
+// Set the DHT11 parameters
+#define DHT11_PIN 7             // USE PIN 7 Digital for the DHT11 temp sensor
+#define DHTTYPE DHT11           // DHT 11 for actual use
 DHT dht(DHT11_PIN, DHTTYPE);
 
-// Degine the I2C Display parameters
+// Set the I2C Display parameters
 #define I2C_ADDR    0x27
 #define LCD_COLUMNS 20
 #define LCD_LINES   4
 
 LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
-//LiquidCrystal_I2C lcd(0x27, 20, 4); // I2C address 0x27, 20 column and 4 rows
 
-// Define Input Pins. These are analog inputs from 0-5v
-int Read_Voltage  = A3;
-int Reset = A1;
+// Define Input Pins. 
+const int ResetPin = 8;          // This is the reset button pin. Using 10k resistor to gnd on the pin 8 side, then the other side of the button to 5v
+const int LoadPin = 12;           // This is the digital button input for 4-8 ohm switching
 
 // Voltage variables
 
-  float FinalRMSVoltage;
-  float Voltage_Value;
+  //float FinalRMSVoltage;
+  //float Voltage_Value;
 
-const float acsOut =A2; // Analog Pin 2
-int acsSensitivity =59;
-float voltage =-1;
-float vRms=0;
+const float acsOut =A2;     // Analog Pin 2 input for the current reading
+int acsSensitivity =59;     // Sets the sensitivity of the current sensor, this should default to 59
+//float voltage =-1;
+//float vRms=0;
 float ampRms=0;
 float watt=0;
 float resistance=0;
 
+int resetValue1 = LOW;      // Defines the base reset value. Once the pin goes high, that will trigger the reset
+float Max_Value;            // Define the Peak value outside of the loop so the state can be reset
 
-const int buttonPin = 8; // This is the reset button pin. Using 10k resistor to gnd on the pin 8 side, then the other side of the button to 5v
-int resetValue1 = LOW; // Defines the base reset value. Once the pin goes high, that will trigger the reset
-float Max_Value; // Define the Peak value outside of the loop so the state can be reset
-
-int button1 = 12; //This is the button input for 4-8 ohm switching
-
-
-void setup() {
-
-  //lcd.begin(20, 4); //Initialise 20*4 LCD
+void setup() 
+{
   lcd.init();
-  lcd.backlight(); // Turn on the backlight
-
+  lcd.backlight();                // Turn on the backlight
   lcd.setCursor(2, 1);
-    lcd.print(" MATTS AMP DYNO"); //Intro Message line 1
-
+    lcd.print(" MATTS AMP DYNO"); // Intro Message line 1
   lcd.setCursor(2, 2);
-    lcd.print("      V1.0  "); //Intro Message line 2
+    lcd.print("      V1.2  ");    // Intro Message line 2
 
-  delay(3000); // Set the intro message display length
-  lcd.clear(); // Clear the display before moving to the data reading
+  delay(3000);                    // Set the intro message display length
+  lcd.clear();                    // Clear the display before moving to the data reading
 
-  pinMode(buttonPin, INPUT); // Set the reset button to an input
+  pinMode(ResetPin, INPUT);      // Set the reset button to an input
+  pinMode(LoadPin, INPUT);       // Set the reset button to an input
 
-  dht.begin();
+  dht.begin();                   // Initialize the DHT Sensor
 }
-
-
 
 float getVpp()
 {
 float result;
-
 int readValue;
 int maxValue=0;
 int minValue=1023;
@@ -102,37 +92,34 @@ return result;
 void loop() {
 
  // Assign variables for the analog input readings so they can be manipulated
- float Voltage_Value = analogRead(Read_Voltage);
- //float Current_Value = analogRead(Read_Current);
+ //float Voltage_Value = analogRead(Read_Voltage);
+ float Current_Value = analogRead(Read_Current);
  float Temp_Value = dht.readTemperature();
 
 // Set a static amperage value since it will need to be calculated
  float Amperage_Value = 0;
-// Basic constraint for voltage 0-1023 corresponds to 0v-100v
 
 // Calculate a basic current value - NEEDS IMPROVEMENT
-
-//Amperage_Value = (Current_Value - 510) * 5 / 1024 / 0.10 - 0.54;
+// 0.185v(185mV) is rise in output voltage when 1A current flows at input.
+// This value might change depending on the situation, adjust as necessiary per sensor
+Amperage_Value = (2.5 - (Current_Value * (5 / 1024)) )/0.185;   
 
 // Calculate the actual celcius temperature based off the 0-1023 input
 float fahrenheit = ((Temp_Value * 9) + 3) / 5 + 32;
 
-  FinalRMSVoltage = map(Voltage_Value,400,490,0,125);
-
+FinalRMSAmp = map(Current_Value,400,490,0,125);
 
 if (digitalRead(button1) == HIGH)
 {
   resistance = 4;
-  
 }
-if (digitalRead(button1) == LOW)
+else if (digitalRead(button1) == LOW)
 {
   resistance = 8;
-  
 }
 
-float voltage =getVpp();
-vRms =(voltage/2.0)*0.707;
+//float voltage =getVpp();
+//vRms =(voltage/2.0)*0.707;
 ampRms =(vRms*1000)/acsSensitivity;
 watt = ampRms * resistance;
 
